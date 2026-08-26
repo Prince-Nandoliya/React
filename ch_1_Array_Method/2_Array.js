c5d3f371d0eaa765e80eefdc02e2af12c9606776
prince <princenandoliya@gmail.com>
2026-08-26 12:15:55 +0530

    some array method code add

---
+++ ch_1_Array_Method/2_Array.js
@@ -0,0 +1,26 @@
+let number = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log("num array", number);
+
+console.log("after flat method", number.flat());
+
+let Bike = ["GT 650", "BMW S 1000 RR", "Kawasaki ZX-10R", "Suzuki Hayabusa"];
+
+console.log("Bike", Bike);
+
+console.log("after slice method", Bike.slice(2, 5));
+
+console.log("index of", Bike.indexOf("Kawasaki ZX-10R"));
+
+console.log("includes method", Bike.includes("Suzuki Hayabusa"));
+
+
+let num = [2,3,5,6,7,4,1,]
+
+console.log(num)
+
+console.log("after sort method",num.sort())
