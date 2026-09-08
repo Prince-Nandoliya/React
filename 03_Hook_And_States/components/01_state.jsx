0285a941f5126706a4fcd9753ac24e0dede52793
prince <princenandoliya@gmail.com>
2026-09-08 11:07:26 +0530

    implement previous state for multiple updates

--- 03_Hook_And_States/components/01_state.jsx
+++ 03_Hook_And_States/components/01_state.jsx
@@ -1,22 +1,56 @@
+// import { useState } from "react";
+
+// const States = () => {
+//   const [count, setCount] = useState(0);
+
+//   const handleIncrement = () => {
+//     setCount(count + 1);
+//   };
+
+//   const handledecrement =() =>{
+//     setCount(count -1)
+//   }
+
+//   return (
+//     <>
+//       <h1>count value {count}</h1>
+
+//       <button onClick={handleIncrement}>increment +1</button><br /><br />
+//       <button onClick={handledecrement}>increment -1</button>
+//     </>
+//   );
+// };
+
+// export default States;
+
 import { useState } from "react";
 
 const States = () => {
   const [count, setCount] = useState(0);
 
-  const handleIncrement = () => {
-    setCount(count + 1);
+  const handledecrement = () => {
+    setCount((prevCount) => prevCount + 1);
   };
-
-  const handledecrement =() =>{
-    setCount(count -1)
-  }
 
   return (
     <>
-      <h1>count value {count}</h1>
+      <h1>count value :{count}</h1>
 
-      <button onClick={handleIncrement}>increment +1</button><br /><br />
-      <button onClick={handledecrement}>increment -1</button>
+      <button
+        onClick={() => {
+          handledecrement();
+          handledecrement();
+          handledecrement();
+          handledecrement();
+          handledecrement();
+        }}
+      >
+        increment +5
+      </button>
+      <br />
+      <br />
+
+      <button onClick={handledecrement}>increment +1</button>
     </>
   );
 };
