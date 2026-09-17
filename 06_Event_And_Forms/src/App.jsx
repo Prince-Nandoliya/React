65895d373b09df46f46fe8a89e12b80a37d4426f
prince <princenandoliya@gmail.com>
2026-09-17 16:01:52 +0530

    Props code add

---
+++ 06_Event_And_Forms/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Props_01 from './components/01_Props'
+
+const App = () => {
+  return (
+    <>
+    <Props_01 name={"prince"}/>
+    </>
+  )
+}
+
+export default App
---
+++ 06_Event_And_Forms/src/components/01_Props.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Props_01 = ({name}) => {
+  return (
+    <>
+    <h1>name:- {name}</h1>
+    </>
+  )
+}
+
+export default Props_01
