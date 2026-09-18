7e429918dc6c3f7ce1671442614bf0f7854baab2
prince <princenandoliya@gmail.com>
2026-09-18 11:30:44 +0530

    validation code add

--- 06_Event_And_Forms/src/App.jsx
+++ 06_Event_And_Forms/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Props_01 from "./components/01_Props";
 import Conditional_Rendering_02 from "./components/02_Conditional_Rendering";
 import Conditional_Rendering_03 from "./components/03_Conditional_Rendering";
+import Validation_06 from "./components/06_Validation";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Props_01 name={"prince"} />
       <Conditional_Rendering_02 />
       <Conditional_Rendering_03/>
+      <Validation_06/>
     </>
   );
 };
